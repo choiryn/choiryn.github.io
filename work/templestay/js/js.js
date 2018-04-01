2a85c6c751db9dabb2a91f4f3ced489e7390f745
choiryn <37022290+choiryn@users.noreply.github.com>
2018-04-01 18:20:08 +0900

    form

--- work/templestay/js/js.js
+++ work/templestay/js/js.js
@@ -122,3 +122,13 @@ $(document).ready(function(){
 
 // //////////////////////////////////////////////////////
 
+
+// Get the modal
+var modal = document.getElementById('form');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}

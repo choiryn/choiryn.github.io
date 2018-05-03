ad46b16cc1537dbb41d603a22963c2177b10b7d9
choiryn <mamiedoreen@naver.com>
2018-05-03 21:33:09 +0900

    text animation

--- templestay/js/main.js
+++
@@ -1,9 +0,0 @@
-/*header slide*/
-$(document).ready(function(){
-    $(".main_menu").mouseover(function(){
-    	$(".sub,.menuBg").not(":animated").slideDown(700);
-    $(".menuBg").mouseleave(function(){
-    	$(".sub,.menuBg").not(":animated").slideUp(700);
-    	});
-	});
-});

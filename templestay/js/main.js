0b166c55ac1104f1a41e91e41c862b1f20f154d6
choiryn <mamiedoreen@naver.com>
2018-05-03 19:48:43 +0900

    visual

---
+++ templestay/js/main.js
@@ -0,0 +1,14 @@
+ /*드롭메뉴*/
+    // $('.main_menu').on('mouseenter', function () {
+    //     $('.menuBg').css({ display: 'block' });
+    // });
+
+    // $('.menuBg').on('mouseleave', function () {
+    //     $('.menuBg').css({ display: 'none' });
+    // });
+    // $(document).ready(function(){
+    // 	$(".main_menu").mouseenter(function(){
+    // 	$(".sub").slideDown();
+    // 	});
+    // });
+    // 

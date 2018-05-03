b87375cd5a087c6296f7220ca534c0265fcc03f2
choiryn <mamiedoreen@naver.com>
2018-05-03 20:13:20 +0900

    transform

--- templestay/js/main.js
+++ templestay/js/main.js
@@ -1,14 +1,9 @@
- /*드롭메뉴*/
-    // $('.main_menu').on('mouseenter', function () {
-    //     $('.menuBg').css({ display: 'block' });
-    // });
-
-    // $('.menuBg').on('mouseleave', function () {
-    //     $('.menuBg').css({ display: 'none' });
-    // });
-    // $(document).ready(function(){
-    // 	$(".main_menu").mouseenter(function(){
-    // 	$(".sub").slideDown();
-    // 	});
-    // });
-    // 
+/*header slide*/
+$(document).ready(function(){
+    $(".main_menu").mouseover(function(){
+    	$(".sub,.menuBg").not(":animated").slideDown(700);
+    $(".menuBg").mouseleave(function(){
+    	$(".sub,.menuBg").not(":animated").slideUp(700);
+    	});
+	});
+});

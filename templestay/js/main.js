bb246941aac1e4733f3b5823117d87b6fc6b3b6d
choiryn <37022290+choiryn@users.noreply.github.com>
2018-05-05 12:43:18 +0900

    jquery

--- templestay/js/main.js
+++ templestay/js/main.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
-	$(".main_menu").mouseover(function(){
-	   	$(".sub,.menuBg").not(":animated").slideDown(300);
-	});
-	$(".menuBg").mouseleave(function(){
-	    $(".sub,.menuBg").not(":animated").slideUp(300);
-	});
-	$(window).ready(function(){
-	    $(".visual_text").animate({"top":"40%","opacity":"1"},1200);
-	});
-});
+// $(document).ready(function(){
+// 	$(".main_menu").mouseover(function(){
+// 	   	$(".sub,.menuBg").not(":animated").slideDown(300);
+// 	});
+// 	$(".menuBg").mouseleave(function(){
+// 	    $(".sub,.menuBg").not(":animated").slideUp(300);
+// 	});
+// 	$(window).ready(function(){
+// 	    $(".visual_text").animate({"top":"40%","opacity":"1"},1200);
+// 	});
+// });
+
+		//     $(".main_menu").mouseover(function(){
+		//     	$(".sub,.menuBg").not(":animated").slideDown(300);
+		//     });
+		//     $(".menuBg").mouseleave(function(){
+		//     	$(".sub,.menuBg").not(":animated").slideUp(300);
+		//     });
+		//     $(window).scroll(function(){
+		//     	var par = $(window).scrollTop();
+		//     	if(par>=400){
+		//     		$(".con1 h1").animate({"marginRight":"0","opacity":"1"},2000);
+		//     		$(".con1 p").fadeIn(1500);
+		//     		$(".media").delay(500).animate({"opacity":"1","top":"330px"},800);
+		//     	}
+		//     });
+		//     $(window).ready(function(){
+		//     	$(".visual_text").animate({"top":"40%","opacity":"1"},1200);
+		//     });
+		// 

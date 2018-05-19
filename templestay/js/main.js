de891af6461008576e13cc9637bdadac633dffbc
choiryn <37022290+choiryn@users.noreply.github.com>
2018-05-20 04:50:00 +0900

    a

--- templestay/js/main.js
+++ templestay/js/main.js
@@ -179,16 +179,16 @@ $(document).ready(function(){
 	});
 
 	// mobile
-	var slide_sw = true;
+	var slide_sw = $(".mo_sub_depth").css("display");
 	$(".open_slide").click(function(){
 		$(this).find(".mo_sub_depth").slideToggle(300);
-		slide_sw = !slide_sw;
 		if(slide_sw) {
-			$(this).find(".open_icon").text("+");
-		}
-		else {
 			$(this).find(".open_icon").text("-");
 		}
+		else {
+			$(this).find(".open_icon").text("+");
+		}
+		slide_sw = !slide_sw;
 	});
 
 	//mobile_slide

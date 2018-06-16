d39920e84d4c9bda9cac735835264935cb97df9b
choiryn <37022290+choiryn@users.noreply.github.com>
2018-06-16 19:51:27 +0900

    star

--- js/main.js
+++ js/main.js
@@ -11,13 +11,22 @@ $(document).ready(function(){
 		else {
 			clearInterval(constellation);
 		}
-	}, 300);
+	}, 200);
+
+	setTimeout( function() { afterAni(); }, 5200 );
 
 	function strokeAni(a) {
-		$("#constellation_box .line").eq(a).delay(2000).animate({"stroke-dashoffset":"0"},400);
+		$("#constellation_box .line").eq(a).delay(2000).animate({"stroke-dashoffset":"0"},300);
 	}
 
-	$(".intro_text").delay(7300).animate({"bottom":"10px","opacity":"1"},1500);
+	function afterAni()
+	 {
+		$("#constellation_box .line").animate({"opacity":"0.2"},2000);
+
+		$(".intro_text").animate({"bottom":"10px","opacity":"1"},1500);
+	}
+
+	
 });
 
 

8ca14acb0c9f21f6f1cf6d2ffa5cfd83c38e7113
choiryn <37022290+choiryn@users.noreply.github.com>
2018-07-30 18:24:06 +0900

    skill

--- js/main.js
+++ js/main.js
@@ -16,6 +16,13 @@ $(document).ready(function() {
 
 	setTimeout( function() { afterAni(); }, 5200 );
 
+	function afterAni()
+	 {
+		$("#constellation_box .line").animate({"opacity":"0"},1800);
+		$(".opc").animate({"opacity":"0.5"},1000);
+		// $(".intro_text").animate({"bottom":"10px","opacity":"1"},1200);
+	}
+
 	//line animation
 
 	var num = 0;
@@ -29,29 +36,42 @@ $(document).ready(function() {
 		}
 	}, 200);
 
+	function strokeAni(a) {
+		$("#constellation_box .line").eq(a).delay(1800).animate({"stroke-dashoffset":"0"},300);
+	}
+
 	//menu open
 
 	$(".gnb").click(function() {
 		$(".gnb_open").addClass("active");
 	});
 
-
 	// menu close
 
 	$(".gnb_open").click(function() {
 		$(this).removeClass("active");
 	});
+
+	//
+
+	var chartHt, docuHt = 0;
+
+	$(window).on("scroll",function() {
+		
+		chartHt = $(".skill").offset().top;
+		docuHt = $(document).scrollTop();
+
+		if(docuHt >= chartHt) {
+			$(".logo").css("fill","#fff");
+		}
+		else {
+			$(".logo").css("fill","#1b1b1d");
+		}
+	});	
 });
 
-	function strokeAni(a) {
-		$("#constellation_box .line").eq(a).delay(1800).animate({"stroke-dashoffset":"0"},300);
-	}
 
-	function afterAni()
-	 {
-		$("#constellation_box .line").animate({"opacity":"0"},1800);
-		$(".opc").animate({"opacity":"0.5"},1000);
-		// $(".intro_text").animate({"bottom":"10px","opacity":"1"},1200);
-	}
+
+
 
 
